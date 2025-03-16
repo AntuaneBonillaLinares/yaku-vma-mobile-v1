b004890d66ba680f60f21b5ea203269cc4f29666
Antuane Bonilla <u20100920@utp.edu.pe>
2025-03-16 13:43:50 -0500

    Modificación

--- app/src/main/java/com/yaku/yaku_vma/presentation/view/screens/home/Home.kt
+++ app/src/main/java/com/yaku/yaku_vma/presentation/view/screens/home/Home.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.yaku.yaku_vma.R
 import com.yaku.yaku_vma.presentation.viewmodel.screens.home.HomeViewModel
 
@@ -17,16 +18,26 @@ class Home : Fragment() {
 
     private val viewModel: HomeViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnGenerar = view.findViewById<Button>(R.id.btnGenerar)
+
+        btnGenerar.setOnClickListener {
+            // Inflar acta.xml
+            val actaView = layoutInflater.inflate(R.layout.fragment_acta, null)
+
+            // Obtener el contenedor actual del fragmento
+            val parent = view.parent as? ViewGroup
+            parent?.removeAllViews()
+            parent?.addView(actaView)
+        }
     }
 }
---
+++ app/src/main/java/com/yaku/yaku_vma/presentation/viewmodel/screens/home/ActaViewModel.kt
@@ -0,0 +1,7 @@
+package com.yaku.yaku_vma.presentation.viewmodel.screens.home
+
+import androidx.lifecycle.ViewModel
+
+class ActaViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
